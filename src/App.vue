fc67b957782a3c5d15b81399eb2aa31bfc1b2dcd
Daniel Diewald <diewald.d02@htlwienwest.at>
2023-10-12 23:27:26 +0200

    ui changes

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,14 @@ function darkmodeswitch() {
 
 <template>
   <div class="row">
-    <div class="col text-left">
+    <div class="col">
+      <p class="text-brand q-ml-md q-mt-lg">
+        <span>🦉</span> Benedikt | your WhatsApp assistant
+      </p>
+    </div>
+    <div class="col-4 text-right">
       <q-btn
-        class="q-ml-md q-mt-md text-brand"
+        class="q-mt-md q-mr-lg text-brand"
         flat
         round
         @click="darkmodeswitch()"
@@ -27,10 +32,6 @@ function darkmodeswitch() {
         <q-icon v-if="$q.dark.isActive == true" name="light_mode"></q-icon
       ></q-btn>
     </div>
-
-    <p class="text-brand q-mt-lg q-mr-lg text-right col">
-      Benedikt | your WhatsApp assistant
-    </p>
   </div>
 
   <div class="q-pa-md" style="width: 100%">
