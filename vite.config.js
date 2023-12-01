35d7637d594f7740a8ce50e5fb9b7a7b8f89dbb4
Daniel Diewald <diewald.d02@htlwienwest.at>
2023-12-01 10:13:59 +0100

    PWA 1

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './webmanifest.js';
 
 export default defineConfig({
   server: {
     port: 80,
   },
   plugins: [
+    VitePWA({ registerType: 'autoUpdate', manifest }),
     vue({
       template: { transformAssetUrls },
     }),
